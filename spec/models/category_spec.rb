14e9667b5b3a3371bf9254fdc8f9492719287e81
Abdoul <abdoulramanediallo44@gmail.com>
2023-10-16 11:25:15 +0000

    fixing linter error

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,13 +3,13 @@ require 'rails_helper'
 RSpec.describe Category, type: :model do
   it 'is valid with valid attributes' do
     user = User.new(name: 'john', email: 'john@example.com', password: 'password')
-    category = Category.new(name: 'Groceries', icon: 'icon.png', user: user)
+    category = Category.new(name: 'Groceries', icon: 'icon.png', user:)
     expect(category).to be_valid
   end
 
   it 'is not valid without a name' do
     user = User.new(name: 'john', email: 'john@example.com', password: 'password')
-    category = Category.new(icon: 'icon.png', user: user)
+    category = Category.new(icon: 'icon.png', user:)
     expect(category).to_not be_valid
   end
 end
--- spec/models/purchase_spec.rb
+++ spec/models/purchase_spec.rb
@@ -3,15 +3,15 @@ require 'rails_helper'
 RSpec.describe Purchase, type: :model do
   it 'is valid with valid attributes' do
     user = User.new(name: 'john', email: 'john@example.com', password: 'password')
-    category = Category.new(name: 'Groceries', icon: 'icon.png', user: user)
-    purchase = Purchase.new(name: 'Grocery shopping', amount: 50.0, user: user, categories: [category])
+    category = Category.new(name: 'Groceries', icon: 'icon.png', user:)
+    purchase = Purchase.new(name: 'Grocery shopping', amount: 50.0, user:, categories: [category])
     expect(purchase).to be_valid
   end
 
   it 'is not valid without a name' do
     user = User.new(name: 'john', email: 'john@example.com', password: 'password')
-    category = Category.new(name: 'Groceries', icon: 'icon.png', user: user)
-    purchase = Purchase.new(amount: 50.0, user: user, categories: [category])
+    category = Category.new(name: 'Groceries', icon: 'icon.png', user:)
+    purchase = Purchase.new(amount: 50.0, user:, categories: [category])
     expect(purchase).to_not be_valid
   end
 end
