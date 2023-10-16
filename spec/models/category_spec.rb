20bad92f6108755fb89d01da98e1bce86603527f
Abdoul <abdoulramanediallo44@gmail.com>
2023-10-16 10:57:40 +0000

    fixing linter error

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Category, type: :model do
   it 'is valid with valid attributes' do
     user = User.new(name: 'john', email: 'john@example.com', password: 'password')
-    category = Category.new(name: 'Groceries', user: user)
+    category = Category.new(name: 'Groceries', user:)
     expect(category).to be_valid
   end
 
