65163b242c5408fd3b10bb60e301dcf98a66bff9
Abdoul <abdoulramanediallo44@gmail.com>
2023-10-16 10:00:45 +0000

    adding migration to the databse

---
+++ db/migrate/20231016091900_create_purchases.rb
@@ -0,0 +1,10 @@
+class CreatePurchases < ActiveRecord::Migration[7.0]
+  def change
+    create_table :purchases do |t|
+      t.decimal :amount
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
